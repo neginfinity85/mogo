818232255b9db98b099c76a2796b6ae2e3e4fe9e
neginfinity85 <neginfinity85@gmail.com>
2023-10-01 19:16:22 +0200

    Update index.html and style.css to BEM methodology

--- js/app.js
+++ js/app.js
@@ -1,75 +1,71 @@
 $(function () {
-  let introH = $('#intro').innerHeight();
-  let header = $('#header');
-  let scrollOffset = $(window).scrollTop();
-  // let aboutHeight = $('#about').innerHeight();
+	let introH = $('#intro').innerHeight();
+	let header = $('#header');
+	let scrollOffset = $(window).scrollTop();
+	// let aboutHeight = $('#about').innerHeight();
 
-  /* Fixed Header show */
-  checkScroll(scrollOffset);
+	/* Fixed Header show */
+	checkScroll(scrollOffset);
 
-  $(window).on('scroll', function () {
-    scrollOffset = $(this).scrollTop();
+	$(window).on('scroll', function () {
+		scrollOffset = $(this).scrollTop();
 
-    checkScroll(scrollOffset);
-  });
+		checkScroll(scrollOffset);
+	});
 
-  function checkScroll(scrollOffset) {
-    if (scrollOffset >= introH) {
-      header.addClass('fixed');
-    } else {
-      header.removeClass('fixed');
-    }
-    /*     if (scrollOffset <= introH + aboutHeight) {
-      $('#nav a').removeClass('active');
-      $('[data-about]').addClass('active');
-    } */
-  }
+	function checkScroll(scrollOffset) {
+		if (scrollOffset >= introH) {
+			header.addClass('fixed');
+		} else {
+			header.removeClass('fixed');
+		}
+	}
 
-  /* Smooth scroll */
-  $('[data-scroll]').on('click', function (event) {
-    event.preventDefault();
+	/* Smooth scroll */
+	$('[data-scroll]').on('click', function (event) {
+		event.preventDefault();
 
-    let $this = $(this);
-    let blockId = $(this).data('scroll');
-    let blockOffset = $(blockId).offset().top;
+		let $this = $(this);
+		let blockId = $(this).data('scroll');
+		let blockOffset = $(blockId).offset().top;
 
-    $('#nav a').removeClass('active');
-    $this.addClass('active');
-    $('#nav').toggleClass('active');
-    $('#nav-toggle').toggleClass('active');
+		$('#nav a').removeClass('active');
+		$this.addClass('active');
+		$('#nav').toggleClass('active');
+		$('#nav-toggle').toggleClass('active');
 
-    $('html, body').animate(
-      {
-        scrollTop: blockOffset,
-      },
-      500,
-    );
-  });
+		$('html, body').animate(
+			{
+				scrollTop: blockOffset,
+			},
+			500,
+		);
+	});
 
-  /*  Menu nav toggle */
-  $('#nav-toggle').on('click', function (event) {
-    event.preventDefault();
+	/*  Menu nav toggle */
+	$('#nav-toggle').on('click', function (event) {
+		event.preventDefault();
 
-    $(this).toggleClass('active');
-    $('#nav').toggleClass('active');
-  });
+		$(this).toggleClass('active');
+		$('#nav').toggleClass('active');
+	});
 
-  /* Collapse */
-  $('[data-collapse]').on('click', function (event) {
-    event.preventDefault();
+	/* Collapse */
+	$('[data-collapse]').on('click', function (event) {
+		event.preventDefault();
 
-    let $this = $(this);
-    let blockId = $(this).data('collapse');
+		let $this = $(this);
+		let blockId = $(this).data('collapse');
 
-    $this.toggleClass('active');
-    // $(blockId).slideToggle();
-  });
+		$this.toggleClass('active');
+		// $(blockId).slideToggle();
+	});
 
-  /* Slider */
-  $('[data-slider]').slick({
-    infinite: true,
-    fade: false,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  });
+	/* Slider */
+	$('[data-slider]').slick({
+		infinite: true,
+		fade: false,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+	});
 });
